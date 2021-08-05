a0c2f35895b2790f3cbb1e3e4f2e474d280f705f
wangzhiyuan02 <wangzhiyuan02@baidu.com>
2021-08-05 14:47:35 +0800

    使用java15编译

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
-    id("org.jetbrains.compose") version "0.2.0-build132"
+    kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.compose") version "0.4.0"
 }
 
 group = "me.wangzhiyuan"
@@ -31,6 +31,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "KotlinKey"
+            packageVersion = "1.0.0"
         }
     }
 }
